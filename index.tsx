6cf994bcbbb44a5b2d610024daf6fde3abac263c
Limon Monte <limon.monte@gmail.com>
2021-07-22 14:21:18 +0300

    bump deps

--- index.tsx
+++ index.tsx
@@ -2,20 +2,22 @@ import React from 'react'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
-const MySwal = withReactContent(Swal)
+const ReactSwal = withReactContent(Swal)
+
+const ReactSwalWithInput = ReactSwal.mixin({
+  input: 'text',
+  confirmButtonText: <i>OK</i>
+})
 
 async function sayHi() {
-  await MySwal.fire(<i>Hi from webpack!</i>, `SweetAlert2 version: ${Swal.version}`)
+  await ReactSwal.fire(<i>Hi from webpack!</i>, `SweetAlert2 version: ${Swal.version}`)
 
-  const {value: name} = await MySwal.fire({
+  const {value: name} = await ReactSwalWithInput.fire({
     text: 'What is your name?',
-    input: 'text',
-    confirmButtonText: <i>OK</i>
   })
 
-  const {value: location} = await MySwal.fire({
+  const {value: location} = await ReactSwalWithInput.fire({
     html: <strong>Where are you from?</strong>,
-    input: 'text'
   })
 
   await Swal.fire(`Hi ${name} from ${location}!`, '', 'success')
