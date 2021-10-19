de2d0b3ec18750bd80a5eba9df64788572fcf435
Limon Monte <limon.monte@gmail.com>
2021-10-19 17:50:29 +0300

    Use dark theme

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,17 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       }
     ]
   },
