61b29983e7479a1e1170cd0945a505332f938609
Limon Monte <limonte@users.noreply.github.com>
2021-10-19 17:50:29 +0300

    Use dark theme

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 import withReactContent from 'sweetalert2-react-content'
+import './styles.scss'
 
 const ReactSwal = withReactContent(Swal)
 
